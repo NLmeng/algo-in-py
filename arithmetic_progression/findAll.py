fbe079fe00fe81a1060bea96237ef1e1b91c7ed0
Lymeng Naret <lymengnaret@Lymengs-MacBook-Air.local>
2022-10-14 11:04:08 -0700

    refactored

--- arithmetic_progression/findAll.py
+++ arithmetic_progression/findAll.py
@@ -56,25 +56,3 @@ arr = [4, 10, 13, 19, 28, 31, 40, 52]
 print("The missing elements are", findAll(arr, 9));
 #
 #
-
-
-
-# overview: solve for c, if nth position is at mid, then the missing is right else left
-# O(logn)
-def findOne(subArr, n):
-    if n == 2:
-        return (subArr[0] + subArr[1])/2
-    
-    low  = 0
-    high = n-1
-    c    = min(subArr[1] - subArr[0], subArr[2] - subArr[1])
-
-    while low < high-1:
-        mid = (low + high) // 2
-
-        if subArr[mid] - subArr[low] == c * (mid - low):
-            low = mid
-        else:
-            high = mid
-
-    return subArr[low] + c
